cc9196f3f0fc6a7dcc8fbb2c140e87c368ea8621
David Pederson <dpederson@figure.com>
2023-04-18 19:52:49 -0600

    Clean up feature toggle context more

--- lib/dapp/use_case/toggle_ctx.ex
+++ lib/dapp/use_case/toggle_ctx.ex
@@ -7,25 +7,30 @@ defmodule Dapp.UseCase.ToggleCtx do
   # Again, we assume the total number of features is fairly small.
   def enabled?(args, feature_name, toggle_name) do
     find(args.features, feature_name)
-    |> and_then(fn feature ->
-      toggle_enabled?(feature.toggles, toggle_name)
-    end)
+    |> map(fn feature -> toggle_enabled?(feature.toggles, toggle_name) end)
+    |> get_or_else(false)
   end
 
   # Determine if a toggle is enabled.
   defp toggle_enabled?(toggles, name) do
     find(toggles, name)
-    |> and_then(fn toggle ->
-      toggle.enabled
-    end)
+    |> map(fn toggle -> toggle.enabled end)
+    |> get_or_else(false)
   end
 
   # Pass a struct to a predicate when non-nil or else return false
-  defp and_then(struct, f) do
-    if is_nil(struct) do
-      false
-    else
+  defp map(struct, f) do
+    unless is_nil(struct) do
       f.(struct)
+    end
+  end
+
+  # Return a value, or a default when value is nil.
+  defp get_or_else(value, default) do
+    if is_nil(value) do
+      default
+    else
+      value
     end
   end
 
