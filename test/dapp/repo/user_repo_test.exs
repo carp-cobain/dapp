1f92d8e209cc72773f346d6d581d1103cbdea14a
David Pederson <dpederson@figure.com>
2023-04-29 17:10:03 -0600

    Configure SQL sandbox

--- test/dapp/repo/user_repo_test.exs
+++ test/dapp/repo/user_repo_test.exs
@@ -1,19 +1,23 @@
 defmodule Dapp.Repo.UserRepoTest do
   use ExUnit.Case, async: true
-  alias Dapp.Repo.UserRepo, as: Repo
+
+  alias Dapp.Repo.UserRepo
+  alias Ecto.Adapters.SQL.Sandbox
   alias Nanoid.Configuration, as: NanoidConfig
 
   # Test context
   setup do
+    # When using a sandbox, each test runs in an isolated, independent transaction
+    :ok = Sandbox.checkout(Dapp.Repo)
     %{address: "tp18vd8fpwxzck93qlwghaj6arh4p7c5n89x8kskz"}
   end
 
   # Test user repo
   describe "UserRepo" do
     test "should create and get a user using a blockchain address", ctx do
-      user = Repo.create!(ctx.address)
+      user = UserRepo.create!(ctx.address)
       assert String.length(user.id) == NanoidConfig.default_size()
-      assert Repo.get(ctx.address) == user
+      assert UserRepo.get(ctx.address) == user
     end
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
+# Set the sandbox to the correct ownership mode.
+# This ensures that only using a single connection for each test.
+Ecto.Adapters.SQL.Sandbox.mode(Dapp.Repo, :manual)
 ExUnit.start()
