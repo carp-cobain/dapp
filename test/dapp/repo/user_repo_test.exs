e96a35459405af4c3b9fbda397db0767925a3bc7
David Pederson <dpederson@figure.com>
2023-04-30 11:01:18 -0600

    Add access checks to user repo test

--- test/dapp/repo/user_repo_test.exs
+++ test/dapp/repo/user_repo_test.exs
@@ -1,7 +1,9 @@
 defmodule Dapp.Repo.UserRepoTest do
   use ExUnit.Case, async: true
 
+  alias Dapp.Repo
   alias Dapp.Repo.UserRepo
+  alias Dapp.Schema.{Grant, Role}
   alias Ecto.Adapters.SQL.Sandbox
   alias Nanoid.Configuration, as: NanoidConfig
 
@@ -10,7 +12,11 @@ defmodule Dapp.Repo.UserRepoTest do
     # When using a sandbox, each test runs in an isolated, independent transaction
     # which is rolled back after test execution.
     :ok = Sandbox.checkout(Dapp.Repo)
-    %{address: "tp18vd8fpwxzck93qlwghaj6arh4p7c5n89x8kskz"}
+
+    %{
+      address: "tp18vd8fpwxzck93qlwghaj6arh4p7c5n89x8kskz",
+      role: "Test"
+    }
   end
 
   # Test user repo
@@ -18,7 +24,20 @@ defmodule Dapp.Repo.UserRepoTest do
     test "should create and get a user using only a blockchain address", ctx do
       user = UserRepo.create!(ctx.address)
       assert String.length(user.id) == NanoidConfig.default_size()
+      assert is_nil(user.name) && is_nil(user.email)
       assert UserRepo.get(ctx.address) == user
+    end
+
+    test "should return unauthorized for users without a grant", ctx do
+      user = UserRepo.create!(ctx.address)
+      assert UserRepo.access(user) == :unauthorized
+    end
+
+    test "should return authorized role for users with a grant", ctx do
+      user = UserRepo.create!(ctx.address)
+      role = Repo.insert!(%Role{name: ctx.role})
+      Repo.insert!(%Grant{user: user, role: role})
+      assert UserRepo.access(user) == {:authorized, ctx.role}
     end
   end
 end
